e6843ace7beacf59a5a4480a8a3da5fd12d5d89e
James Lannon <jamesmlannon1990@live.com>
2017-05-08 14:05:09 -0700

    add styles, create js functions to display newTrip properties when clicked on

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //business logic
 function Trip(location, bestFood, favoriteMemory, year, season) {
   this.loc = location;
-  this.grub = food;
+  this.grub = bestFood;
   this.mem = favoriteMemory;
   this.time = year;
   this.weather = season;
@@ -13,26 +13,32 @@ Trip.prototype.spot = function() {
 
 //user interface logic
 
-$(function() {
+$(document).ready(function() {
   $("form#new-trip").submit(function(event) {
+    event.preventDefault();
 
-  var inputtedLocation = $("input#new-location").val();
-  var inputtedFood = $("input#new-food").val();
-  var inputtedMemory = $("input#new-memory").val();
-  var inputtedYear = $("input#new-year").val();
-  var inputtedSeason = $("input#new-season").val();
+    var inputtedLocation = $("input#new-location").val();
+    var inputtedFood = $("input#new-food").val();
+    var inputtedMemory = $("input#new-memory").val();
+    var inputtedYear = $("input#new-year").val();
+    var inputtedSeason = $("input#new-season").val();
 
-  var newTrip = new Trip(inputtedLocation, inputtedFood, inputtedMemory, inputtedYear, inputtedSeason);
+    var newTrip = new Trip(inputtedLocation, inputtedFood, inputtedMemory, inputtedYear, inputtedSeason);
 
-  $("ul#trips").append("<li><span class='trip'>" + newTrip.spot() + "</span></li>");
+    $("ul#trips").append("<li><span class='trip'>" + newTrip.spot() + "</span></li>");
 
-
+    $(".trip").last().click(function() {
+    $("#show-trip").show();
+    $("#show-trip h2").text(newTrip.loc);
+    $(".location").text(newTrip.loc);
+    $(".food").text(newTrip.grub);
+    $(".memory").text(newTrip.mem);
+    $(".year").text(newTrip.time);
+    $(".season").text(newTrip.weather);
 
   });
+    $("input#new-location").val("");
+    $("input#new-food").val("");
 
-
-
-
-
-
+  });
 });
