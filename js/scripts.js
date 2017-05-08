c71137f29da15db362e372ca297522e616335571
James Lannon <jamesmlannon1990@live.com>
2017-05-08 13:14:42 -0700

    add files and directories, add business logic creating an object Trip and it's prototype spot, pull html from addressbook project

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+//business logic
+function Trip(location, bestFood, favoriteMemory, year, season) {
+  this.loc = location;
+  this.grub = food;
+  this.mem = favoriteMemory;
+  this.time = year;
+  this.weather = season;
+}
+
+Trip.prototype.spot = function() {
+  return this.loc;
+}
+
+//user interface logic
+
+$(function() {
+  $()
+
+
+
+
+
+
+});
